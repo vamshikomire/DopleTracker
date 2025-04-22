eb1a7d9b2ab04b98ddb97c704f4dba116949c9d1
komire vamshi <154540718+vamshikomire@users.noreply.github.com>
2025-04-22 15:04:35 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+st.markdown("""
+    <div style="text-align: center;">
+        <p>Micro-Doppler Target Classification System</p>
+        <div style="margin-top: 15px; padding: 10px; background-color: rgba(70, 70, 70, 0.1); border-radius: 5px; display: inline-block;">
+            <p style="font-size: 0.95em; margin: 0; font-weight: bold;">Developed by:</p>
+            <p style="font-size: 0.9em; margin-top: 8px;">Vamshi Komire, 217Y1A3357</p>
+            <p style="font-size: 0.9em; margin-top: 5px;">Sri Charan Reddy, 217Y1A3352</p>
+        </div>
+    </div>
+    """,
+            unsafe_allow_html=True)
